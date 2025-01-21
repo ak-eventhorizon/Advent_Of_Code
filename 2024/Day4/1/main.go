61fa00afb42168cd18396abdea62976a063ee564
ak-eventhorizon <45125426+ak-eventhorizon@users.noreply.github.com>
2025-01-21 09:10:41 +0300

    Day 4 updated

--- 2024/Day4/1/main.go
+++ 2024/Day4/1/main.go
@@ -12,7 +12,7 @@ import (
 
 func main() {
 
-	data := GetMatrix("data.txt")
+	data := GetMatrix("data2.txt")
 	pattern := "XMAS"
 
 	answer := day4_1(data, pattern)
@@ -22,24 +22,177 @@ func main() {
 func day4_1(matrix [][]string, pattern string) int {
 
 	var result int
-
-	// TODO
+	var allWays []string // все строки, столбцы, прямые и обратные диагонали из матрицы в виде строк
 
 	// добавить к набору все строки из матрицы
-	// set append GetLines(matrix)
+	// allWays = append(allWays, GetLines(matrix)...)
 
 	// добавить к набору все столбцы из матрицы
-	// set append GetColumns(matrix)
+	// allWays = append(allWays, GetColumns(matrix)...)
 
 	// добавить к набору все прямые диагонали из матрица
-	// set append GetForwardDiag(matrix)
+	// allWays = append(allWays, GetForwardDiag(matrix)...)
 
 	// добавить к набору все обратные диагонали из матрицы
-	// set append GetBackwardDiag(matrix)
+	allWays = append(allWays, GetBackwardDiag(matrix)...)
 
 	// искать в каждом элемента набора pattern и перевернутый pattern
 	// strings.Count(elem, "XMAS"))
 	// strings.Count(elem, "SAMX"))
+
+	fmt.Println(allWays)
+
+	return result
+}
+
+// Функция извлекает из матрицы все строки
+func GetLines(matrix [][]string) []string {
+	var result []string
+
+	sizeX := len(matrix[0]) // размерность матрицы по оси X (количество столбцов)
+	sizeY := len(matrix)    // размерность матрицы по оси Y (количество строк)
+
+	for y := 0; y < sizeY; y++ {
+		var line string
+		for x := 0; x < sizeX; x++ {
+			line += matrix[y][x]
+		}
+		result = append(result, line)
+	}
+
+	return result
+}
+
+// Функция извлекает из матрицы все столбцы
+func GetColumns(matrix [][]string) []string {
+	var result []string
+
+	sizeX := len(matrix[0]) // размерность матрицы по оси X (количество столбцов)
+	sizeY := len(matrix)    // размерность матрицы по оси Y (количество строк)
+
+	for x := 0; x < sizeX; x++ {
+		var column string
+		for y := 0; y < sizeY; y++ {
+			column += matrix[y][x]
+		}
+		result = append(result, column)
+	}
+
+	return result
+}
+
+// Функция извлекает из матрицы все прямые диагонали /
+func GetForwardDiag(matrix [][]string) []string {
+	var result []string
+
+	// последовательность сбора диагоналей
+	// 		0 1 2 3
+	// 		1 2 3 4
+	// 		2 3 4 5
+	// 		3 4 5 6
+	// 		4 5 6 7
+	// 		5 6 7 8
+
+	sizeX := len(matrix[0]) // размерность матрицы по оси X (количество столбцов)
+	sizeY := len(matrix)    // размерность матрицы по оси Y (количество строк)
+
+	// перебор диагоналей начинающихся от первого столбца (0-5)
+	for line := 0; line < sizeY; line++ {
+		var diagonal string
+
+		x := 0
+		y := line
+
+		for {
+			diagonal += matrix[y][x]
+			y--
+			x++
+
+			if (x == sizeX) || (y == -1) { //выход за границы матрицы
+				break
+			}
+		}
+
+		result = append(result, diagonal)
+	}
+
+	// перебор диагоналей начинающихся от нижней строки (6-8)
+	for column := 1; column < sizeX; column++ {
+		var diagonal string
+
+		x := column
+		y := sizeY - 1
+
+		for {
+			diagonal += matrix[y][x]
+			y--
+			x++
+
+			if (x == sizeX) || (y == -1) { //выход за границы матрицы
+				break
+			}
+		}
+
+		result = append(result, diagonal)
+	}
+
+	return result
+}
+
+// Функция извлекает из матрицы все обратные диагонали \
+func GetBackwardDiag(matrix [][]string) []string {
+	var result []string
+
+	// последовательность сбора диагоналей
+	// 		3 2 1 0
+	// 		4 3 2 1
+	// 		5 4 3 2
+	// 		6 5 4 3
+	// 		7 6 5 4
+	// 		8 7 6 5
+
+	sizeX := len(matrix[0]) // размерность матрицы по оси X (количество столбцов)
+	sizeY := len(matrix)    // размерность матрицы по оси Y (количество строк)
+
+	// перебор диагоналей начинающихся от последнего столбца (0-5)
+	for line := 0; line < sizeY; line++ {
+		var diagonal string
+
+		x := sizeX - 1
+		y := line
+
+		for {
+			diagonal = matrix[y][x] + diagonal // диагональ заполняется справа налево
+			y--
+			x--
+
+			if (x == -1) || (y == -1) { //выход за границы матрицы
+				break
+			}
+		}
+
+		result = append(result, diagonal)
+	}
+
+	// перебор диагоналей начинающихся от нижней строки (6-8)
+	for column := sizeX - 2; column >= 0; column-- {
+		var diagonal string
+
+		x := column
+		y := sizeY - 1
+
+		for {
+			diagonal = matrix[y][x] + diagonal // диагональ заполняется справа налево
+			y--
+			x--
+
+			if (x == -1) || (y == -1) { //выход за границы матрицы
+				break
+			}
+		}
+
+		result = append(result, diagonal)
+	}
 
 	return result
 }
