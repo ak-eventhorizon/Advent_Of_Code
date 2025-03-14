4fe0ede767c4ece12ec8a83e94a24c48625019be
ak-eventhorizon <45125426+ak-eventhorizon@users.noreply.github.com>
2025-03-14 09:11:28 +0300

    Research progress save

--- 2024/Day8/1/main.go
+++ 2024/Day8/1/main.go
@@ -25,12 +25,72 @@ func main() {
 
 func day8_1(layout [][]string) (result int) {
 
-	puzzle := new(Field)
-	puzzle.state = slices.Clone(layout)
+	// resField := slices.Clone(layout) // копия исходного поля, для установки на него вычисленных точек
 
-	fmt.Println(puzzle.state)
+	antennas := make(map[string][][]int) // структура для хранения всех видов антенн с координатами - map[A:[[8 1] [5 2] [7 3]] B:[[6 5] [9 9]]]
+
+	// обход поля для составления списка всех антенн с их координатами
+	for y, line := range layout {
+		for x, name := range line {
+			if name != "." {
+				// проверка - существует ли уже в перечне антенна такого вида
+				if _, isPresent := antennas[name]; isPresent { // добавить текущую к существующему набору
+					antennas[name] = append(antennas[name], []int{x, y})
+				} else { // создать набор антенн такого вида и добавить к нему текущую
+					antennas[name] = make([][]int, 0)
+					antennas[name] = append(antennas[name], []int{x, y})
+				}
+			}
+		}
+	}
+
+	// обход всех видов антенн. по каждому виду построение отражения (антиноды) для каждой антенны от каждой другой
+	for k, v := range antennas {
+		antinodes := findAllAntiNodes(v)
+		fmt.Println(k, " --> ", v, "--antinodes-->", antinodes) // DEBUG print
+		// нарисовать антиноды на resField --> #
+	}
 
 	return result
+}
+
+// Функция получает набор антенн [x,y], возвращает набор антинод для входного набора антенн, вычисленных по условиям задачи
+func findAllAntiNodes(antennas [][]int) (antinodes [][]int) {
+
+	for i := range antennas {
+
+		tmp := slices.Clone(antennas)
+		currentAntenna := antennas[i]
+		restAntens := slices.Delete(tmp, i, i+1)
+
+		for _, secondAntenna := range restAntens {
+			antinodes = append(antinodes, reflect(currentAntenna, secondAntenna))
+			antinodes = append(antinodes, reflect(secondAntenna, currentAntenna))
+		}
+
+	}
+
+	return antinodes
+}
+
+// Функция отражает точку p2[x2,y2] относительно точки p1[x1,y1] и возвращает координаты отраженной точки [xr,yr]
+func reflect(p1, p2 []int) (r []int) {
+
+	x1 := p1[0]
+	y1 := p1[1]
+
+	x2 := p2[0]
+	y2 := p2[1]
+
+	deltaX := x1 - x2
+	deltaY := y1 - y2
+
+	r = make([]int, 2)
+
+	r[0] = x1 + deltaX
+	r[1] = y1 + deltaY
+
+	return r
 }
 
 // Функция извлекает из текстового файла карту задачи.
