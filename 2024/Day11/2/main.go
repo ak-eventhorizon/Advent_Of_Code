7167848249ca6ecb750505028ae488ef909a9d46
ak-eventhorizon <45125426+ak-eventhorizon@users.noreply.github.com>
2025-04-01 12:26:14 +0300

    Day 11_2 complete

--- 2024/Day11/2/main.go
+++ 2024/Day11/2/main.go
@@ -4,7 +4,6 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"slices"
 	"strconv"
 	"strings"
 	"time"
@@ -14,81 +13,107 @@ import (
 // --- Day 11: Plutonian Pebbles --- Puzzle 2
 // https://adventofcode.com/2024/day/11
 
+// вводим в программу кэш, который будет хранить количество камней, вычисленное для комбинации "исходный_камень-количество_итераций"
+// например для четырех итераций исходного камня "42" - кэш будет хранить значения:
+// "42-1" --> 2 		["4" "2"]
+// "42-2" --> 2 		["8096" "4048"]
+// "42-3" --> 4 		["80" "96" "40" "48"]
+// "42-4" --> 8 		["8" "0" "9" "6" "4" "0" "4" "8"]
+var cache map[string]int
+
 func main() {
 
-	input := GetData("data.txt")
-	answer := day11_2(input, 25)
+	cache = make(map[string]int) // инициализация кэша
 
+	input := GetData("data.txt")
+	answer := day11_2(input, 75)
 	fmt.Println(answer)
 }
 
-func day11_2(input []string, blinks int) (result int) {
+func day11_2(stones []string, blinks int) (result int) {
 
-	// вводим кэш, который будет хранить для каждого значение - его преобразование по указанным в задаче правилам
-	cache := make(map[string][]string)
+	start := time.Now()
 
-	tmp := slices.Clone(input)
-
-	for i := 0; i < blinks; i++ {
-		start := time.Now()
-
-		tmp = blink(tmp, cache)
-
-		duration := time.Since(start)
-		fmt.Printf("%v -- %s \n", i, duration) // фиксация времени выполнения итерации
+	for _, stone := range stones {
+		result += count_stone_blinks(stone, blinks)
 	}
 
-	return len(tmp)
+	fmt.Printf("%s \n", time.Since(start)) // замер времени выполнения
+	return result
 }
 
-// Функция производит одну итерацию манипуляций с набором данных input по следующим правилам:
-// 1) Если элемент = "0", он заменяется на "1"
-// 2) Если количество знаков элемента четное "2000" - оно разбивается на две равные части "20" и "00", лидирующие нули убираются -> "20" и "0"
-// 3) В осталных случаях - значение элемента умножается на 2024
-// cache - общая между всеми итерациями кеширующая структура, сохраняющая рпезультаты каждого преобразования
-func blink(input []string, cache map[string][]string) (result []string) {
+// Функция вычисляет во сколько камней превратится текущий камень stone через depth итерций --
+// Рекурсия с кэшированием результата
+func count_stone_blinks(stone string, depth int) (result int) {
 
-	for _, elem := range input {
+	// проверка - присутствуют ли данные в кэше
+	key := stone + "-" + strconv.Itoa(depth)
+	v, isCacheContainKey := cache[key]
 
-		if cache[elem] == nil { // пишем результат преобразования в кэш, если его еще там нет
+	if isCacheContainKey {
+		return v
+	} else {
+		left_stone, right_stone := single_blink_stone(stone)
 
-			if elem == "0" {
-				cache[elem] = []string{"1"}
-			} else if len(elem)%2 == 0 {
-				s := strings.Split(elem, "")
-
-				part1 := s[:len(elem)/2]
-				p1 := strings.Join(part1, "")
-
-				// избавляемся от лидирующих нулей конвертацией Atoi -> Itoa
-				part2 := s[len(elem)/2:]
-				val, err := strconv.Atoi(strings.Join(part2, ""))
-				if err != nil {
-					panic(err)
-				}
-
-				p2 := strconv.Itoa(val)
-
-				cache[elem] = []string{p1, p2}
+		// условие выхода из рекурсии
+		if depth == 1 {
+			if right_stone == "" {
+				return 1
 			} else {
-				val, err := strconv.Atoi(elem)
-
-				if err != nil {
-					// fmt.Println("ding", elem) // DEBUG
-					panic(err)
-				}
-
-				val *= 2024
-				s := strconv.Itoa(val)
-
-				cache[elem] = []string{s}
+				return 2
+			}
+		} else {
+			result = count_stone_blinks(left_stone, depth-1)
+			if right_stone != "" {
+				result += count_stone_blinks(right_stone, depth-1)
 			}
 		}
 
-		result = append(result, cache[elem]...)
+		cache[key] = result
+		return result
+	}
+}
+
+// Функция производит один шаг превращения камня stone и возвращает камни (leftStone, rightStone), в которые он превратился
+// Второго камня (rightStone) может не быть, в этом случае вместо него возвращается пустая строка
+// Правила превращения:
+// 1) Если элемент = "0", он заменяется на "1"
+// 2) Если количество знаков элемента четное "2000" - он разбивается на две равные части "20" и "00", лидирующие нули убираются -> "20" и "0"
+// 3) В осталных случаях - значение элемента умножается на 2024
+func single_blink_stone(stone string) (leftStone, rightStone string) {
+
+	if stone == "0" {
+		leftStone = "1"
+		rightStone = ""
+	} else if len(stone)%2 == 0 {
+		s := strings.Split(stone, "")
+
+		part1 := s[:len(stone)/2]
+		leftStone = strings.Join(part1, "")
+
+		// избавляемся от лидирующих нулей конвертацией Atoi <-> Itoa
+		part2 := s[len(stone)/2:]
+		val, err := strconv.Atoi(strings.Join(part2, ""))
+		if err != nil {
+			panic(err)
+		}
+
+		rightStone = strconv.Itoa(val)
+
+	} else {
+		val, err := strconv.Atoi(stone)
+
+		if err != nil {
+			panic(err)
+		}
+
+		val *= 2024
+
+		leftStone = strconv.Itoa(val)
+		rightStone = ""
 	}
 
-	return result
+	return leftStone, rightStone
 }
 
 // Функция извлекает из файла filename исходняе данные для задачи
