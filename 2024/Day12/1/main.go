95ae6138fd3bc43f1e3f3c4c6378bfce52e17785
ak-eventhorizon <45125426+ak-eventhorizon@users.noreply.github.com>
2025-04-03 12:25:50 +0300

    Day 12_1 complete

--- 2024/Day12/1/main.go
+++ 2024/Day12/1/main.go
@@ -17,18 +17,9 @@ import (
 func main() {
 	start := time.Now()
 
-	// input := GetData("data.txt")
-	// answer := day12_1(input)
-	// fmt.Println(answer)
-
-	tmp := [][]string{}
-	tmp = append(tmp, []string{"O", "O", "O", "O", "O"})
-	tmp = append(tmp, []string{"O", "X", "O", "X", "O"})
-	tmp = append(tmp, []string{"O", "O", "O", "O", "O"})
-	tmp = append(tmp, []string{"O", "X", "O", "X", "O"})
-	tmp = append(tmp, []string{"O", "O", "O", "O", "O"})
-	tmp2 := extractRegions(tmp)
-	fmt.Println(tmp2)
+	input := GetData("data.txt")
+	answer := day12_1(input)
+	fmt.Println(answer)
 
 	fmt.Printf("%s \n", time.Since(start)) // измерение времени выполнения функции
 }
@@ -37,7 +28,7 @@ func day12_1(input [][]string) (result int) {
 
 	os.Truncate("output.txt", 0) //очистка файла
 
-	gardens := extractIdenticalGardens(input)
+	gardens := extractRegions(input)
 
 	for _, garden := range gardens {
 		transformed := transformToWeights(garden)
@@ -137,21 +128,28 @@ func transformToWeights(matrix [][]string) (result [][]string) {
 
 // Функция извлекает обособленные регионы из исходной матрицы
 //
-// .....    ...   result["O-0"]    result["X-0"]    result["X-1"]
+// .....    ...     result[0]        result[1]        result[2]
 // OOOOO    -->      OOOOO            .....            .....
 // OXXOO    -->      O..OO            .XX..            .....
 // OOOOO    -->      OOOOO            .....            .....
 // OOOXO    -->      OOO.O            .....            ...X.
 // OOOOO    -->      OOOOO            .....            .....
-func extractRegions(matrix [][]string) (result map[string][][]string) {
+func extractRegions(matrix [][]string) (result map[int][][]string) {
 
-	result = map[string][][]string{}
+	result = map[int][][]string{}
+	counter := 0
 
 	lenY := len(matrix)
 	lenX := len(matrix[0])
 
+	//делаем копию исходной матрицы, чтобы не вносить в нее изменения; далее работаем с копией
+	var copyMatrix [][]string
+	for _, line := range matrix {
+		copyMatrix = append(copyMatrix, slices.Clone(line))
+	}
+
 	//анализ каждой клетки исходного поля
-	for y, line := range matrix {
+	for y, line := range copyMatrix {
 		for x, char := range line {
 
 			// пропускаем клетку, если она пустая
@@ -169,99 +167,104 @@ func extractRegions(matrix [][]string) (result map[string][][]string) {
 				blankMatrix = append(blankMatrix, line)
 			}
 
-			// поиском в ширину вычиляются все координаты региона
+			// поиском в ширину вычиляются все координаты региона текущей клетки
 			start := []int{x, y}
 			currentCells := [][]int{}
 			currentCells = append(currentCells, start)
-			visitedCells := [][]int{}
-			visitedCells = append(visitedCells, start)
+			regionCoords := [][]int{}
+			regionCoords = append(regionCoords, start)
 
-			for i := 1; i <= 9; i++ { // TODO цикл изменить на бесконечный с условием выхода
-				currentCells = stepBFS(currentCells, matrix)
-				visitedCells = append(visitedCells, currentCells...)
+			// пока следующий шаг вычислений находит соседей - продолжаем вычисление
+			for len(currentCells) > 0 {
+				currentCells = stepBFS(currentCells, copyMatrix)
+				regionCoords = append(regionCoords, currentCells...)
 			}
 
-			// сохраняем регион записью в мап
+			for _, coord := range regionCoords {
+				x := coord[0]
+				y := coord[1]
+				blankMatrix[y][x] = char // сохраняем регион в чистую матрицу
+				copyMatrix[y][x] = "."   // в исходной поле заменяем все клетки найденного региона на "." чтобы больше их не анализировать
+			}
 
-			// в исходной поле очищаем все клетки найденного региона на "." чтобы больше их не анализировать
-
+			// добавление региона к результату
+			result[counter] = blankMatrix
+			counter++
 		}
 	}
 
 	return result
 }
 
+// Функция получает набор координат и возвращает набор координат всех соседних клеток (по горизонтали и вертикали), имеющих идентичнео содержимое
+// Реализует один шаг алгоритма поиска в ширину (Breadth-First Search)
 func stepBFS(currentCells [][]int, matrix [][]string) (nextCells [][]int) {
+
+	for _, cell := range currentCells {
+		x := cell[0]
+		y := cell[1]
+		nextCells = append(nextCells, calcNextStep(x, y, matrix)...)
+	}
+
+	// удалению дублей, поскольку одна клетка может быть достигнута на этом шаге несколькими разными путями
+	nextCells = removeDuplicates(nextCells)
 
 	return nextCells
 }
 
-// На основе координат исходной клетки x,y вычисляется весь регион клеток с таким же значением, соединенный по вертикали и горизонтали
-func calcNextStep(px, py int, matrix [][]string) (result [][]int) {
+// На основе координат исходной клетки x,y вычисляются коордираты ее соседей с таким же значением по вертикали и горизонтали
+func calcNextStep(x, y int, matrix [][]string) (result [][]int) {
 
-	regionName := matrix[py][px]
+	regionName := matrix[y][x] // сохраняем содержимое клетки (имя региона)
+	matrix[y][x] = "."         // очищаем клетку, как обработанную
 	lenY := len(matrix)
 	lenX := len(matrix[0])
-	currentStep := [][]int{}
-	currentStep = append(currentStep, []int{px, py})
-	nextStep := [][]int{}
 
-	for _, c := range currentStep {
-		x := c[0]
-		y := c[1]
-
-		// проверка соседа сверху
-		if y-1 >= 0 && matrix[y-1][x] == regionName {
-			nextStep = append(nextStep, []int{x, y - 1})
-		}
-		// проверка соседа снизу
-		if y+1 < lenY && matrix[y+1][x] == regionName {
-			nextStep = append(nextStep, []int{x, y + 1})
-		}
-		// проверка соседа слева
-		if x-1 >= 0 && matrix[y][x-1] == regionName {
-			nextStep = append(nextStep, []int{x - 1, y})
-		}
-		// проверка соседа справа
-		if x+1 < lenX && matrix[y][x+1] == regionName {
-			nextStep = append(nextStep, []int{x + 1, y})
-		}
+	// проверка соседа сверху
+	if y-1 >= 0 && matrix[y-1][x] == regionName {
+		result = append(result, []int{x, y - 1})
+	}
+	// проверка соседа снизу
+	if y+1 < lenY && matrix[y+1][x] == regionName {
+		result = append(result, []int{x, y + 1})
+	}
+	// проверка соседа слева
+	if x-1 >= 0 && matrix[y][x-1] == regionName {
+		result = append(result, []int{x - 1, y})
+	}
+	// проверка соседа справа
+	if x+1 < lenX && matrix[y][x+1] == regionName {
+		result = append(result, []int{x + 1, y})
 	}
 
 	return result
 }
 
-// TODO -- Удалить после завершения функции extractRegions
-// Функция разбирает исходную матрицу на структуру нескольких матриц, каждая из которых содержит карту только своего вида
-func extractIdenticalGardens(matrix [][]string) (result map[string][][]string) {
+// Функция удаляет дубли из произвольного слайса слайсов целых чисел
+func removeDuplicates(source [][]int) (result [][]int) {
 
-	result = map[string][][]string{}
+	// превращение слайса слайсов интов в слайс строк, который можно отсортировать и удалить дубли в сортированном
+	strSlice := []string{}
+	for _, elem := range source {
+		strElem := strings.Trim(strings.Replace(fmt.Sprint(elem), " ", "-", -1), "[]") // [3 4] --> "3-4"
+		strSlice = append(strSlice, strElem)
+	}
 
-	lenY := len(matrix)
-	lenX := len(matrix[0])
+	slices.Sort(strSlice)               // сортировка
+	strSlice = slices.Compact(strSlice) // удаление дубликатов
 
-	//анализ каждой клетки исходного поля
-	for y, line := range matrix {
-		for x, char := range line {
-
-			_, isKeyExistInMap := result[char]
-
-			// если такого ключа в мапе еще нет - его нужно создать и заполнить пустой матрицей
-			if !isKeyExistInMap {
-				// создаем пустую матрицу (заполненную ".") по размеру исходной, на которую будем наносить каждый вид сада и сохранять его отдельно
-				var blankMatrix [][]string
-				for range lenY {
-					line := []string{}
-					for range lenX {
-						line = append(line, ".")
-					}
-					blankMatrix = append(blankMatrix, line)
-				}
-				result[char] = slices.Clone(blankMatrix)
+	// превращение слайса строк обратно в слайс слайсов интов
+	for _, s := range strSlice {
+		elements := strings.Split(s, "-")
+		element := []int{}
+		for _, v := range elements {
+			num, err := strconv.Atoi(v)
+			if err != nil {
+				panic(err)
 			}
-
-			result[char][y][x] = char
+			element = append(element, num)
 		}
+		result = append(result, element)
 	}
 
 	return result
